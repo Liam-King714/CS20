08b2c16e02fe5df47228a8fd1e8408036872ff75
510228002 <510228002@L815WD220027.edu.cbe.ab.ca>
2026-09-10 09:50:57 -0600

    9:50 AM

---
+++ chapter3/src/skillBuilders/rectangle.java
@@ -0,0 +1,32 @@
+package skillBuilders;
+
+import java.util.Scanner;
+
+public class rectangle 
+{
+
+	public static void main(String[] args) 
+	{
+		//Declare Variables
+		int length;
+		int width;
+		
+		//Create a Scanner object
+		Scanner userinput = new Scanner(System.in);
+		
+		//Get width from the keyboard
+		System.out.print("Enter the width: ");
+		width = userinput.nextInt();
+		
+		//Get length from the keyboard
+		System.out.print("Enter the length: ");
+		length = userinput.nextInt();
+		
+		
+		//Display the width and length on the console/screen
+		System.out.println("The width is: " + width);
+		System.out.print("The length is: " + length);
+		
+	}
+
+}
